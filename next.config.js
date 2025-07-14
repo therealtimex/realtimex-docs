a59a29f3ba3540a0a29e6ca8391d12a0acd56679
Trung Le <trungle@rta.vn>
2025-07-14 13:58:21 -0700

    build

--- next.config.js
+++ next.config.js
@@ -1,17 +1,31 @@
+// next.config.js
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
-})
+});
+
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || ''; // e.g. '/realtimex-docs' on GH Pages
 
 module.exports = withNextra({
+  output: 'export',
+  basePath,
+  assetPrefix: basePath,
+  trailingSlash: true,
+  reactStrictMode: true,
+  swcMinify: true,
+
+  images: {
+    unoptimized: true,
+  },
+
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
     });
-
     return config;
   },
+
   async redirects() {
     return [
       {
